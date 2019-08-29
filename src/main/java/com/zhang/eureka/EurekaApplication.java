c72241b6ad04e42b51d2bcb58940098ca024f9c3
1013524743@qq.com <1013524743@qq.com>
2019-08-29 15:13:57 +0800

    第一次提交

---
+++ src/main/java/com/zhang/eureka/EurekaApplication.java
@@ -0,0 +1,19 @@
+package com.zhang.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author zhen
+ * 启动尤里卡server   即是server端，又是client端
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
