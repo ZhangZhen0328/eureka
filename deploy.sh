507b653abb2c961f052c60d1f6efa1b3d0f04c0f
1013524743@qq.com <1013524743@qq.com>
2019-09-04 10:53:08 +0800

    修改sh

--- deploy.sh
+++ deploy.sh
@@ -1,33 +1,25 @@
 #!/usr/bin/env bash
 
-killtomcat(){
-    pid=`ps -ef|grep tomcat|grep java|awk '${print $2}'`
+killEureka(){
+    pid=`ps -ef|grep eureka.jar`
     echo "tomcat Id list :$pid"
     if [ "$pid" = "" ]
     then
-        echo "no tomcat pid alive"
+        echo "no eureka pid alive"
     else
         kill -9 $pid
     fi
 }
+
 cd $PROJ_PATH/eureka
 mvn clean install
 
-#停止tomcat
-killtomcat
-
-# 删除原有工程
-rm -rf $TOMCAT_APP_PATH/webapps/ROOT
-rm -f $TOMCAT_APP_PATH/webapps/ROOT.jar
-rm -f $TOMCAT_APP_PATH/webapps/eureka.jar
+#杀死eureka的服务
+killEureka
 
 # 复制新的工程
-cp $PROJ_PATH/eureka/target/eureka.war $TOMCAT_APP_PATH/webapps/
+cp $PROJ_PATH/eureka/target/eureka.jar /root/eureka/
 
-cd $TOMCAT_APP_PATH/webapps/
-mv eureka.war ROOT.war
+nohup java -jar /root/eureka/eureka.jar &
 
-# 启动tomcat
-cd $TOMCAT_APP_PATH/
-sh bin/startup.sh
 
