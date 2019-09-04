13a4a6a952e42aa5a698ec4bf18db6c091ccd752
1013524743@qq.com <1013524743@qq.com>
2019-09-04 14:32:20 +0800

    修改application

--- src/main/java/com/zhang/eureka/EurekaApplication.java
+++ src/main/java/com/zhang/eureka/EurekaApplication.java
@@ -14,7 +14,6 @@ public class EurekaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class, args);
-        System.out.println("启动成功");
     }
 
 }
