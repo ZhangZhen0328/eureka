5ba60564b3fb43026daf3ae8cdbc68c685921b6f
1013524743@qq.com <1013524743@qq.com>
2019-09-04 09:42:46 +0800

    修改pom文件

---
+++ deploy.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+killtomcat(){
+    pid=`ps -ef|grep tomcat|grep java|awk '${print $2}'`
+    echo "tomcat Id list :$pid"
+    if [ "$pid" = "" ]
+    then
+        echo "no tomcat pid alive"
+    else
+        kill -9 $pid
+    fi
+}
+cd $PROJ_PATH/eureka
+mvn clean install
+
+#停止tomcat
+killtomcat
+
+# 删除原有工程
+rm -rf $TOMCAT_APP_PATH/webapps/ROOT
+rm -f $TOMCAT_APP_PATH/webapps/ROOT.jar
+rm -f $TOMCAT_APP_PATH/webapps/order.jar
+
+# 复制新的工程
+cp $PROJ_PATH/eureka/target/order.war $TOMCAT_APP_PATH/webapps/
+
+cd $TOMCAT_APP_PATH/webapps/
+mv order.war ROOT.war
+
+# 启动tomcat
+cd $TOMCAT_APP_PATH/
+sh bin/startup.sh
+
