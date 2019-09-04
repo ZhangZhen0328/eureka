ae1f4450188220d2a5945c5f0c432677fbb43415
1013524743@qq.com <1013524743@qq.com>
2019-09-04 10:58:53 +0800

    修改sh

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,7 @@
 #!/usr/bin/env bash
 
-killEureka(){
-    pid=`ps -ef|grep eureka.jar`
-    echo "tomcat Id list :$pid"
-    if [ "$pid" = "" ]
-    then
-        echo "no eureka pid alive"
-    else
-        kill -9 $pid
-    fi
-}
-
 cd $PROJ_PATH/eureka
-mvn clean install
-
-#杀死eureka的服务
-killEureka
+mvn clean install -Dmaven.test.skip=true
 
 # 复制新的工程
 cp $PROJ_PATH/eureka/target/eureka.jar /root/eureka/
